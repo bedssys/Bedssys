a2853086c50eecf5a99b08203e0ea173361246c1
william chandra <williamchans57@gmail.com>
2019-04-04 12:03:24 +0700

    csharp

---
+++ IPC CS/Main.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace TestSO
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(94, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1020, 450);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button1.Location = new System.Drawing.Point(514, 687);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 34);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Mulai Kamera";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Location = new System.Drawing.Point(183, 492);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(351, 22);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 492);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "URL cam 0";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(94, 534);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "URL cam 1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox2.Location = new System.Drawing.Point(183, 534);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(351, 22);
+            this.textBox2.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(94, 574);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 17);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "URL cam 2";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox3.Location = new System.Drawing.Point(183, 574);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(351, 22);
+            this.textBox3.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(94, 613);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 17);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "URL cam 3";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox4.Location = new System.Drawing.Point(183, 613);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(351, 22);
+            this.textBox4.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(650, 495);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 17);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Face Tolerance";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox5.Location = new System.Drawing.Point(763, 492);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(351, 22);
+            this.textBox5.TabIndex = 17;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(271, 739);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 34);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Save Image";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(763, 739);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(166, 34);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "Save Video";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button4.Location = new System.Drawing.Point(514, 739);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(166, 34);
+            this.button4.TabIndex = 21;
+            this.button4.Text = "Alarm Activate";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(630, 539);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(127, 17);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Security Threshold";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox6.Location = new System.Drawing.Point(763, 539);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(351, 22);
+            this.textBox6.TabIndex = 23;
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button5.Location = new System.Drawing.Point(271, 687);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(166, 34);
+            this.button5.TabIndex = 24;
+            this.button5.Text = "Normal Mode";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button6.Location = new System.Drawing.Point(763, 687);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(166, 34);
+            this.button6.TabIndex = 25;
+            this.button6.Text = "Face Input";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(677, 582);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(80, 17);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "Face Name";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox7.Location = new System.Drawing.Point(763, 579);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(351, 22);
+            this.textBox7.TabIndex = 27;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(1215, 840);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.Text = "IPC";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox7;
+    }
+}
+
---
+++ IPC CS/Main.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestSO
+{
+
+    public partial class Main : Form
+    {
+
+        private readonly object _lock = new object();
+        private readonly Queue<string> _queue = new Queue<string>();
+        private readonly AutoResetEvent _signal = new AutoResetEvent(false);
+        private static NamedPipeServerStream server;
+        private BinaryReader br;
+        private BinaryWriter bw;
+        public Main()
+        {
+            InitializeComponent();
+            server = new NamedPipeServerStream("testing");
+            br = new BinaryReader(server);
+            bw = new BinaryWriter(server);
+            new Thread(new ThreadStart(ProducerThread)).Start();
+        }
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (server != null)
+            {
+                server.Close();
+                server.Dispose();
+                server = null;
+            }
+            Application.Exit();
+        }
+
+        private String mode = "normal";
+        void ProducerThread()
+        {
+            while (true)
+            {
+                _signal.WaitOne();
+                string item = string.Empty;
+                do
+                {
+                    item = string.Empty;
+                    lock (_lock)
+                    {
+                        if (_queue.Count > 0)
+                        {
+                            item = _queue.Dequeue();
+                        }
+                    }
+
+                    if (item != string.Empty)
+                    {
+
+                        try
+                        {
+
+                            if (server != null && !server.IsConnected)
+                                server.WaitForConnection();
+
+                            if (server != null && server.IsConnected)
+                            {
+                                var str = new string(item.ToString().ToArray());
+
+                                var buf = Encoding.ASCII.GetBytes(str);
+                                bw.Write((uint)buf.Length);
+                                bw.Write(buf);
+                            }
+                            if (server != null && server.IsConnected)
+                            {
+                                var len = (int)br.ReadUInt32();
+                                var str = new string(br.ReadChars(len));
+                                if (str == "Image")
+                                {
+                                    searchimage();
+                                } else if (str == "Wait")
+                                {
+                                    lock (_lock)
+                                    {
+                                        while (_queue.Count > 0)
+                                        {
+                                            String throwingaway = _queue.Dequeue();
+                                        }
+                                    }
+                                    _signal.Set();
+                                } 
+                            }
+                        }
+                        catch (Exception EX)
+                        {
+                            MessageBox.Show(EX.Message.ToString());
+                        }
+                    }
+                }
+                while (item != string.Empty);
+            }
+        }
+        private void searchimage()
+        {
+            Image img;
+            using (var bmpTemp = new Bitmap("D:\\skripsi programming\\Bedssys\\display_sharp.jpg"))
+            {
+                img = new Bitmap(bmpTemp);
+            }
+            //load image in picturebox
+            pictureBox1.Image = img;
+            lock (_lock)
+            {
+                _queue.Enqueue("Received");
+            }
+            _signal.Set();
+        }
+        private int checkbutton = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkbutton == 0) {
+                lock (_lock)
+                {
+                    _queue.Enqueue("Start");
+                    String text1 = " ";
+                    String text2 = " ";
+                    String text3 = " ";
+                    String text4 = " ";
+                    String text5 = " ";
+                    String text6 = " ";
+                    if (textBox1.Text != "")
+                    {
+                        text1 = textBox1.Text;
+                    }
+                    if (textBox2.Text != "")
+                    {
+                        text2 = textBox2.Text;
+                    }
+                    if (textBox3.Text != "")
+                    {
+                        text3 = textBox3.Text;
+                    }
+                    if (textBox4.Text != "")
+                    {
+                        text4 = textBox4.Text;
+                    }
+                    if (textBox5.Text != "")
+                    {
+                        text5 = textBox5.Text;
+                    }
+                    if (textBox6.Text != "")
+                    {
+                        text6 = textBox6.Text;
+                    }
+                    _queue.Enqueue(text1);
+                    _queue.Enqueue(text2);
+                    _queue.Enqueue(text3);
+                    _queue.Enqueue(text4);
+                    _queue.Enqueue(text5);
+                    _queue.Enqueue(text6);
+                    _queue.Enqueue("Received");
+                }
+                button1.Text = "Stop Kamera";
+                checkbutton = 1;
+            }
+            else
+            {
+                lock (_lock)
+                {
+                    _queue.Enqueue("Stop");
+                }
+                button1.Text = "Mulai Kamera";
+                checkbutton = 0;
+            }
+            _signal.Set();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if(mode == "normal") {
+                lock (_lock)
+                {
+                    _queue.Enqueue("Normal");
+                }
+                button5.Text = "Recognition Mode";
+                mode = "recognition";
+            }
+            else
+            {
+                lock (_lock)
+                {
+                    _queue.Enqueue("Recognition");
+                }
+                button5.Text = "Normal Mode";
+                mode = "normal";
+            }
+            _signal.Set();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            lock (_lock)
+            {
+                _queue.Enqueue("FaceInput");
+                _queue.Enqueue("FaceName");
+            }
+            _signal.Set();
+        }
+        private int alarm;
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            if (alarm == 0)
+            {
+                lock (_lock)
+                {
+                    _queue.Enqueue("AlarmActive");
+                }
+                button4.Text = "Alarm Activate";
+                alarm = 1;
+            }
+            else
+            {
+                lock (_lock)
+                {
+                    _queue.Enqueue("AlarmDeactive");
+                }
+                button4.Text = "Alarm Deactivate";
+                alarm = 0;
+            }
+            _signal.Set();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lock (_lock)
+            {
+                _queue.Enqueue("SaveImage");
+            }
+            _signal.Set();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            lock (_lock)
+            {
+                _queue.Enqueue("SaveVideo");
+            }
+            _signal.Set();
+        }
+    }
+}
