5b20b81c745b810a7844eb185cdaee4b3fc604f5
Arsenic <d.faris323@gmail.com>
2019-06-24 18:17:49 +0700

    New model for the 4 camera mode
    Two new models, one is augmented, one is not (augmented is 10x non-augmented). Some small changes:
    - Average used in tracking now ignores points placed in origin (and axis, as an unwanted consequence). To mitigate for undetected points giving zeros.
    - Utilities to overlap gestures before used for training.

---
+++ utilities/pose_overlapper.py
@@ -0,0 +1,50 @@
+dir = "overlap/"
+input = "in.txt"
+output = "out.txt"
+
+npose = 8   # Pose per gesture
+nover = 3   # Overlap
+
+def pose_overlapper():
+    # File handling
+    in_file = dir + input
+    out_file = dir + output
+    
+    fout = open(out_file, 'w').close() # Clear existing file
+    fin = open(in_file, 'r')    # Open in read mode
+    fout = open(out_file, 'a+') # Open in append mode
+    
+    source = []
+    
+    # Read all the source data
+    for x in fin:
+      source.append(x)
+    
+    fin.close()
+    
+    if npose <= 2*nover:
+        return
+    
+    #for n in range(len(source)):    
+    # Modifying the iterator used for "FOR"
+    # isn't legal and not working
+    
+    p = 1   # The actual counter, the first one is 1
+    n = 0   # Source iterator
+    while n < len(source):
+        fout.write(source[n])
+        # fout.write('\n')
+        # Not necessary since the source is a text with newline
+        
+        # Overlap by stepping back the incrementation
+        if p == npose:
+            n -= nover
+            p = 0
+        
+        p += 1
+        n += 1
+    
+    fout.close()
+
+if __name__ == '__main__':
+    pose_overlapper()
